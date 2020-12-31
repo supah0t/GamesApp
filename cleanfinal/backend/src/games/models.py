121c7db53771bca65e8130e67445cb65c60ebcc4
supah0t <supah0t@users.noreply.github.com>
2020-12-31 15:03:17 +0200

    Fix Complete v0

--- cleanfinal/backend/src/games/models.py
+++ cleanfinal/backend/src/games/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class PlayerScore(models.Model):
     user = models.CharField(max_length=40)
-    score = models.PositiveIntegerField(default=0)
+    score = models.DecimalField(max_digits=5, decimal_places=2)
     
     def __str__(self):
         return str(self.score)
